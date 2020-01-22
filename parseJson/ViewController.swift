6598b14c57a5b30c877e9f1cf3c517c7ad001cea
U.M. islam <u.m.hamidulislam@labs-mac.local>
2020-01-22 15:53:21 +0600

    Initial Commit

---
+++ parseJson/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  parseJson
+//
+//  Created by U.M. Hamidul islam on 1/22/20.
+//  Copyright © 2020 Kader. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
